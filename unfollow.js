281d0ac47886b1df1363b0111cd04b07f5efc70b
SentryMNG <90071612+SentryMNG@users.noreply.github.com>
2022-07-01 21:43:08 +0100

    Update unfollow.js

--- unfollow.js
+++ unfollow.js
@@ -4,7 +4,7 @@
 
   const retry = {
     count: 0,
-    limit: 3,
+    limit: 100000,
   };
 
   const scrollToTheBottom = () => window.scrollTo(0, document.body.scrollHeight);
@@ -14,7 +14,7 @@
   const sleep = ({ seconds }) =>
     new Promise((proceed) => {
       console.log(`WAITING FOR ${seconds} SECONDS...`);
-      setTimeout(proceed, seconds * 1000);
+      setTimeout(proceed, seconds * 0);
     });
 
   const unfollowAll = async (followButtons) => {
@@ -22,7 +22,7 @@
     await Promise.all(
       followButtons.map(async (followButton) => {
         followButton && followButton.click();
-        await sleep({ seconds: 1 });
+        await sleep({ seconds: 0 });
         const confirmButton = document.querySelector($confirmButton);
         confirmButton && confirmButton.click();
       })
@@ -31,14 +31,14 @@
 
   const nextBatch = async () => {
     scrollToTheBottom();
-    await sleep({ seconds: 1 });
+    await sleep({ seconds: 0 });
 
     const followButtons = Array.from(document.querySelectorAll($followButtons));
     const followButtonsWereFound = followButtons.length > 0;
 
     if (followButtonsWereFound) {
       await unfollowAll(followButtons);
-      await sleep({ seconds: 2 });
+      await sleep({ seconds: 0 });
       return nextBatch();
     } else {
       addNewRetry();
@@ -48,7 +48,7 @@
       console.log(`NO ACCOUNTS FOUND, SO I THINK WE'RE DONE`);
       console.log(`RELOAD PAGE AND RE-RUN SCRIPT IF ANY WERE MISSED`);
     } else {
-      await sleep({ seconds: 2 });
+      await sleep({ seconds: 0 });
       return nextBatch();
     }
   };
